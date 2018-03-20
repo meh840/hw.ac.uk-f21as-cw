476df95fcca4d583f16df7f903f92e2f39490c3d
Alan Spence <alanspence81@gmail.com>
2018-03-20 21:59:46 +0000

    Small Improvements
    Improved SimulationClock & Plane, Plane still needs work and a Junit tester

--- src/uk/ac/hw/F21AS/GROUPms256as294pt45/CoreStage2/SimulationClock.java
+++ src/uk/ac/hw/F21AS/GROUPms256as294pt45/CoreStage2/SimulationClock.java
@@ -20,8 +20,15 @@ public final class SimulationClock extends Observable implements Runnable {
 	private boolean keepTicking = true;
 	private ArrayList<String> departureTimes = new ArrayList<String>();
 	
+	/**
+	 * Private constructor as using Singleton pattern.
+	 */
 	private SimulationClock() {}
 	
+	/**
+	 * Get an instance to use in the simulation.
+	 * @return An instance of the simulation clock.
+	 */
 	public static SimulationClock GetInstance() {
 		if(clock == null) {
 			clock = new SimulationClock();
@@ -31,10 +38,20 @@ public final class SimulationClock extends Observable implements Runnable {
 		return clock;
 	}
 	
+	/**
+	 * Gives the simulation time in hours and minutes (24hr format).
+	 * @return String giving time as HH:mm
+	 */
 	public String CurrentTime() {
 		return GiveTimeAsString(hours, minutes);
 	}
 	
+	/**
+	 * Given hours and minutes, will give a string of the time.
+	 * @param hoursValue Hours of time 0 - 23
+	 * @param minutesValue Minutes of time 0 - 60
+	 * @return String giving time as HH:mm
+	 */
 	private String GiveTimeAsString(int hoursValue, int minutesValue) {
 		String time = "";
 		
@@ -53,19 +70,55 @@ public final class SimulationClock extends Observable implements Runnable {
 		return time;
 	}
 	
+	/**
+	 * Use to stop the simulation clock from running.
+	 */
 	public void StopTheClock() {
 		keepTicking = false;
 	}
 	
+	/**
+	 * Will take the time till departure to notify of a departure due and will give 
+	 * the time of the departure.
+	 * @param minutesTillDeparture How many minutes until the plane will depart.
+	 * @return String giving time as HH:mm
+	 */ 
 	public String AddDepartureTime(int minutesTillDeparture) {	
-		int departureMinutes = minutes % 60;
-		int departureHours = (hours + (minutes - departureMinutes)) % 24;	
+		int departureMinutes = (minutes + minutesTillDeparture) % 60;
+		int hoursToAdd = ((minutes + minutesTillDeparture) - departureMinutes) / 60;
+		int departureHours = (hours + hoursToAdd) % 24;	
 		
 		String departureTime = GiveTimeAsString(departureHours, departureMinutes);
 		departureTimes.add(departureTime);
 		return departureTime;
 	}
 	
+	/**
+	 * Check if a departure time has been.
+	 * @param departureTime String of time as HH:mm.
+	 * @return True: Departure has been. False: Not yet departed.
+	 */
+	public boolean DepartureCheck(String departureTime) {
+		String[] departure = departureTime.split("\\:");
+		int hrs = Integer.parseInt(departure[0]);
+		int mins = Integer.parseInt(departure[1]);
+		
+		if(hrs < hours) {
+			return false;
+		}
+		
+		if(hrs == hours) {
+			if(mins <= minutes) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Checks if a departure is due and will remove from list and notify change to check with planes.
+	 */
 	private void DepartureDue() {
 		String currentTime = CurrentTime();
 		
@@ -77,6 +130,10 @@ public final class SimulationClock extends Observable implements Runnable {
 		}
 	}
 	
+	/**
+	 * Increases the seconds. If increase reaches 60 seconds, increments the minutes.
+	 * @param numberOfIncreaments How many seconds to increase by.
+	 */
 	private void IncreaseSeconds(int numberOfIncreaments) {
 		for(int c = 0; c < numberOfIncreaments; c++) {
 			seconds++;
@@ -87,6 +144,10 @@ public final class SimulationClock extends Observable implements Runnable {
 		}
 	}
 	
+	/**
+	 * Increases the minutes. If increase reaches 60 minutes, increments the hours.
+	 * @param numberOfIncreaments How many minutes to increase by.
+	 */
 	private void IncreaseMinutes(int numberOfIncreaments) {
 		for(int c = 0; c < numberOfIncreaments; c++) {
 			minutes++;
@@ -97,6 +158,10 @@ public final class SimulationClock extends Observable implements Runnable {
 		}
 	}
 	
+	/**
+	 * Increase the hours. If increase reaches 24 hours, flips back to zero.
+	 * @param numberOfIncreaments How many hours to increase by.
+	 */
 	private void IncreaseHours(int numberOfIncreaments) {
 		for(int c = 0; c < numberOfIncreaments; c++) {
 			hours++;
